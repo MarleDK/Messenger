3a63653135fcab2b9230e766d15a12222f3179a9
Mwthorn <mwthorn@gmail.com>
2016-01-08 09:04:49 +0100

    added try/catch for connection lost when waiting for message. added BufferReader for Inputstream

--- src/Server/main/ClientConnectionThread.java
+++ src/Server/main/ClientConnectionThread.java
@@ -2,15 +2,50 @@ package Server.main;
 
 //Husk at tråden ikke skal kunne køre i bagggrunden, hvis fx. socket er død.
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class ClientConnectionThread extends Thread{
     boolean threadOK;
-    String ID;
+    private Socket socket;
+    //private PrintWriter pw;
+    private BufferedReader br;
+    private String ID;
+    private String Input;
 
-    public ClientConnectionThread() {
-        this.threadOK = true;
+    public ClientConnectionThread(Socket s, String ID) {
+        try {
+            this.socket = s;
+            socket.setSoTimeout(5000);
+            //pw = new PrintWriter(s.getOutputStream());
+            br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            this.ID = ID;
+
+            threadOK = true;
+        } catch (Exception ex) {
+            threadOK = false;
+        }
     }
 
     public void run(){
 
+        try {
+            Input = br.readLine();
+        }
+        catch (Exception ex) {
+            try {
+                socket.shutdownOutput();
+                socket.shutdownInput();
+                socket.close();
+            }
+            catch (Exception ex1) {
+                return;
+            }
+        }
+
+        System.out.println(Input);
     }
+
 }
