e398e9743d4e97c1168d5f016ae64f1f5f87e82e
Mwthorn <mwthorn@gmail.com>
2016-01-08 09:23:38 +0100

    added ToString in Message object

--- src/universalClasses/Message.java
+++ src/universalClasses/Message.java
@@ -2,13 +2,19 @@ package universalClasses;
 
 
 public class Message {
-    String text;
-    TimeStamp time;
-    String samtaleID;
-    String AfsenderID;
+    private String text;
+    private TimeStamp time;
+    private String samtaleID;
+    private String AfsenderID;
 
     public Message(String text, String samtaleID, String Afsender){
 
+    }
+
+    public String toString(String text, TimeStamp time, String Samtale, String AfsenderID) {
+        // Den returnere flere linjer med brug af \n
+        // Starte med en linje kaldt "Message"
+        return null;
     }
 
 }
