7782e819556c8cbf25e5f6fbb43b9b56495aa288
DJ <dejartarikf96@gmail.com>
2016-01-08 10:59:06 +0100

    kckejv

--- src/universalClasses/Message.java
+++ src/universalClasses/Message.java
@@ -2,21 +2,22 @@ package universalClasses;
 
 
 public class Message {
-    private String text;
-    private TimeStamp time;
-    private String samtaleID;
-    private String AfsenderID;
+    public final String text;
+    public final TimeStamp time;
+    public final String samtaleID;
+    public final String AfsenderID;
 
 
-    public Message(TimeStamp time, String text, String samtaleID, String Afsender){
+    public Message(TimeStamp time, String samtaleID, String Afsender, String text){
 
 
     }
 
-    public Message(String text, String samtaleID, String Afsender){
+    public Message(String samtaleID, String Afsender, String text){
         this.text = text;
         this.samtaleID = samtaleID;
         this.AfsenderID = Afsender;
+        this.time = new TimeStamp();
 
     }
 
@@ -24,13 +25,15 @@ public class Message {
         String s = this.text;
 
         for(int i = 0; i< text.length(); i++){
-            if(this.text ){
+            if(s.charAt(i) == '§' ){
+                
+
 
             }
 
         }
 
-        return("Message\n" + this.samtaleID + "\n" + this.AfsenderID + "\n" + time);
+        return("Message\n" + this.time + "\n" + this.samtaleID + "\n" + this.AfsenderID + "\n" + this.text);
 
         // Den returnere flere linjer med brug af \n
         // Starte med en linje kaldt §
@@ -41,6 +44,8 @@ public class Message {
     public String getText(){
         return null;
     }
+    
+    
 
 
 }
