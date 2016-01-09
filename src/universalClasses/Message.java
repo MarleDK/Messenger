558c70e1ee183a35178b5a114446cf11dc07f8ad
MarleDK <jakobrydhof@gmail.com>
2016-01-09 19:31:00 +0100

    Har lavet en toMessage metode, der tager en string, i formen Message.toString, og laver den til en message, og returnerer denne message. Hvis der ikke står "Message" i starten, returnerer den null.

--- src/universalClasses/Message.java
+++ src/universalClasses/Message.java
@@ -17,11 +17,6 @@ public class Message {
 
     }
 
-    public Message(String s){
-        //lav en konstruktør, der tager message.toString teksten, og laver det til en Message
-        while ()
-    }
-
     public Message(String samtaleID, String Afsender, String text){
         this.text = text;
         this.samtaleID = samtaleID;
@@ -45,11 +40,39 @@ public class Message {
 
         }
 
-        return("Message§" + this.time + "§" + this.samtaleID + "§" + this.afsenderID + "§" + this.text);
+        return("Message§" + this.time + "§" + this.samtaleID + "§" + this.afsenderID + "§" + s);
 
         // Den returnere flere linjer med brug af \n
         // Starte med en linje kaldt §
 
+
+    }
+
+    public static Message toMessage(String s){
+        if(!s.startsWith("Message§")){
+            return null;
+        }
+        String data = s.substring(28);
+        String[] txtData = new String[3];
+        int charIndex = 0;
+        for (int i = 0; i < 3; i++) {
+            boolean isToken = true;
+            while (charIndex < data.length() && isToken) {
+                if (data.charAt(charIndex) == '§' && data.charAt(charIndex + 1) == '§') {
+                    txtData[i] += data.charAt(charIndex);
+                    charIndex += 2;
+                } else if (data.charAt(charIndex) == '§') {
+                    charIndex += 1;
+                    isToken = false;
+                } else {
+                    txtData[i] += data.charAt(charIndex);
+                    charIndex += 1;
+                }
+
+            }
+        }
+        TimeStamp time = new TimeStamp(s.substring(8, 27));
+        return new Message(time, txtData[0].substring(4), txtData[1].substring(4), txtData[2].substring(4));
 
     }
 
