ed99d9cf02467c8f90c807bd74f127084564806e
MarleDK <jakobrydhof@gmail.com>
2016-01-10 12:41:48 +0100

    Har lavet en getLogFromServer metode, og gjort så den kun gemmer beskeder i ArrayLists, da det vel egentlig giver bedre mening, hvis ikke clienten, skal gemme dem for hvergang

--- src/Client/main/Main.java
+++ src/Client/main/Main.java
@@ -6,19 +6,20 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import universalClasses.Message;
 import universalClasses.TimeStamp;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
+import java.io.*;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main extends Application {
     private static String currentChat;
     private static String userID;
     private static Socket socket;
+    private static ArrayList<String> chatIDs;
+    private static ArrayList<ArrayList<Message>> chatlogs;
 
     @Override
     public void start(Stage primaryWindow) throws Exception{
@@ -55,9 +56,40 @@ public class Main extends Application {
         }
     }
 
-    private static void getLog(Socket s){
+
+    private static void getLogFromServer() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+
+        br.readLine();
+        chatlogs = new ArrayList<>();
+        int chatLogIndex = 0;
+
+        Boolean hasMoreChats = true;
+        while(hasMoreChats){
+            String chatID = br.readLine();
+            chatIDs.add(chatID);
+            chatlogs.add(new ArrayList<Message>());
+
+            if (chatID.equals("§")) {
+                hasMoreChats = false;
+            } else {
+                Boolean hasMoreMessages = true;
+                while(hasMoreMessages){
+
+                    if (br.readLine().equals("§")) {
+                        hasMoreMessages = false;
+                    } else {
+                        chatlogs.get(chatLogIndex).add(Message.toMessage(br.readLine()));
+                    }
+                }
+            }
+            chatLogIndex++;
+        }
 
     }
+
+
 
     public static String getCurrentChat(){
         return currentChat;
