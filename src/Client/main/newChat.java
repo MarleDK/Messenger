21732f5c583ab135d93113323108e1e0bf0d09d6
Mwthorn <mwthorn@gmail.com>
2016-01-13 09:58:23 +0100

    added listView

--- src/Client/main/newChat.java
+++ src/Client/main/newChat.java
@@ -4,25 +4,26 @@ import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class newChat {
-    @FXML
     private static VBox userListVBox;
 
-    @FXML
     private static ListView<String> users;
-    @FXML
     private static ListView<String> chatUsers;
 
-    @FXML
+    private static int indexChatUser;
+
     public static void makeChat(String clients){
 
         System.out.println("Starter makeChat i newChat...");
@@ -44,64 +45,64 @@ public class newChat {
         Button cancelbtn = new Button();
         cancelbtn.setText("Cancel");
 
-        ListView<String> list = new ListView<String>();
-        ObservableList<String> items = FXCollections.observableArrayList (
-                "Single", "Double", "Suite", "Family App");
-        list.setItems(items);
-        list.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+        users = new ListView<>();
+        ObservableList<String> useritems = FXCollections.observableArrayList ();
+        users.setItems(useritems);
+        // users.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
 
-        NewChatScene.add(list,0,0);
+
+        chatUsers = new ListView<>();
+        ObservableList<String> chatitems = FXCollections.observableArrayList ();
+        chatUsers.setItems(chatitems);
+        // chatUsers.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+        chatUsers.setOnMouseClicked(e -> {
+            if (chatUsers.getSelectionModel().getSelectedItem() != null) {
+                useritems.add(chatUsers.getSelectionModel().getSelectedItem());
+                chatitems.remove(chatUsers.getSelectionModel().getSelectedItem());
+            }
+        });
+        users.setOnMouseClicked(e -> {
+            if (users.getSelectionModel().getSelectedItem() != null) {
+                chatitems.add(users.getSelectionModel().getSelectedItem());
+                useritems.remove(users.getSelectionModel().getSelectedItem());
+            }
+        });
+        submitbtn.setOnAction(e -> {
+
+            Main.getPw().print("NewChat§");
+            for(int i=0; i<chatUsers.getItems().size(); i++){
+                Main.getPw().print(chatUsers.getItems().get(i)+"§");
+            }
+            Main.getPw().println();
+            Main.getPw().flush();
+            // er det kun det man skal gøre?
+        });
+
+        NewChatScene.add(users,0,0);
+        NewChatScene.add(chatUsers,1,0);
         NewChatScene.add(Buttons,0,1);
         Buttons.add(cancelbtn,0,0);
         Buttons.add(submitbtn,1,0);
 
-
-
-//        String client ="";
-//        ArrayList<String> chatUsers = null;
-//        int indexChatUser = 0;
-//        for(int i=0; i < clients.length(); i++) {
-//            if (clients.charAt(i) != '§') {
-//                client += clients.charAt(i);
-//            } else {
-//                System.out.println(client);
-//                users.getItems().add(client);
-//                users.getItems().get(indexChatUser);
-//                users.setOnMouseClicked(e -> {
-//                    if (chatUsers == null) {
-//                        chatUsers.add(e.getSource().toString());
-//                        users.getItems().remove(e.getSource().toString());
-//                    } else if (chatUsers.contains(e.getSource().toString())) {
-//                        chatUsers.remove(e.getSource().toString());
-//                        users.getItems().add(e.getSource().toString());
-//                    } else {
-//                        chatUsers.add(e.getSource().toString());
-//                        users.getItems().remove(e.getSource().toString());
-//                    }
-//
-//                });
-//                client = "";
-//                indexChatUser ++;
-//            }
-//        }
+        String client ="";
+        indexChatUser = 0;
+        for(int i=0; i < clients.length(); i++) {
+            if (clients.charAt(i) != '§') {
+                client += clients.charAt(i);
+            } else {
+                System.out.println(client);
+                useritems.add(client);
+                client = "";
+                indexChatUser ++;
+            }
+        }
         Main.getPrimaryWindow().setScene(new Scene(NewChatScene));
         Alert noget = new Alert(Alert.AlertType.CONFIRMATION);
         noget.showAndWait();
-        items.add("Kage");
 
     }
 
-    @FXML
-    public void makeNewChat(ActionEvent actionEvent) {
-        Main.getPw().print("NewChat§");
-        for(int i=0; i<chatUsers.getItems().size(); i++){
-            Main.getPw().print(chatUsers.getItems().get(i)+"§");
-        }
-        Main.getPw().println();
-        Main.getPw().flush();
-    }
 
-    @FXML
     public void cancelNewChat(ActionEvent actionEvent) {
         chatUsers = null;
 
